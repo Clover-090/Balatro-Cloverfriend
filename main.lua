e026c18ee9445a4f6594a9b3ddb7b17ce900bcbf
Clover-090 <Clover.c21@proton.me>
2025-01-27 13:58:00 -0500

    Fully Added Hypothetical Joker

--- main.lua
+++ main.lua
@@ -67,21 +67,38 @@ SMODS.Joker {
     loc_txt = {
         name = 'Hypothetical Joker',
         text = {
-          "After each Boss Blind",
-          "spawn a hanged man card" --This will change once I'm more knowlegeable of LUA
+          "After each {C:attention}Boss Blind{}",
+          "spawn a {C:attention}negitive hanged man card" --This will change once I'm more knowlegeable of LUA
           
         }
       },
-    config = {},
+    config = {extra = {}},
     rarity = 3,
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
     atlas = 'Clovermod',
     pos = { x = 0, y = 0 }, --TODO: add correct cords after sprites are finished
-    cost = 8,
+    cost = 10,
     unlocked = false,
     unlock_condition = {type = 'win_deck', deck = 'b_checkered'},
+
+    loc_vars = function(self, info_queue, card)
+		info_queue[#info_queue + 1] = G.P_CENTERS.e_negative
+	end,
+
+    calculate = function(self, card, context)
+        if context.end_of_round and G.GAME.blind.boss
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    local card = create_card('Tarot',G.consumeables, nil, nil, nil, nil, nil, c_death)
+                    card:set_edition({negative = true}, true, true)
+                    card:add_to_deck()
+                    G.consumeables:emplace(card)
+					    return true
+				    end
+                end
+            end
 }
 
 
