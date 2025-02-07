7f3c4d727c79d86af4d4efd6b252344f70f171f6
Clover-090 <Clover.c21@proton.me>
2025-02-07 15:30:03 -0500

    Began adding Config menu to enable and disable certain jokers, currenty non-funtional

--- main.lua
+++ main.lua
@@ -1,3 +1,63 @@
+local clov_config = SMODS.current_mod.config
+-- Code Borrowed From Cardsauce mod
+clov_enabled = copy_table(clov_config)
+
+local function config_matching()
+	for k, v in pairs(clov_enabled) do
+		if v ~= clov_config[k] then
+			return false
+		end
+	end
+	return true
+end
+
+
+function G.FUNCS.clov_restart()
+	if config_matching() then
+		SMODS.full_restart = 0
+	else
+		SMODS.full_restart = 1
+	end
+end
+
+
+SMODS.current_mod.config_tab = function()
+	local ordered_config = {
+		'enableMoker',
+		'enableLoker',
+		'enableCameo',
+
+	}
+	local left_settings = { n = G.UIT.C, config = { align = "tm", padding = 0.05 }, nodes = {} }
+	local right_settings = { n = G.UIT.C, config = { align = "tm", padding = 0.05 }, nodes = {} }
+	for i, k in ipairs(ordered_config) do
+		if #right_settings.nodes < #left_settings.nodes then
+			right_settings.nodes[#right_settings.nodes + 1] = create_toggle({ label = localize("clov_options_"..ordered_config[i]), ref_table = clov_config, ref_value = ordered_config[i], callback = G.FUNCS.clov_restart})
+		else
+			left_settings.nodes[#left_settings.nodes + 1] = create_toggle({ label = localize("clov_options_"..ordered_config[i]), ref_table = clov_config, ref_value = ordered_config[i], callback = G.FUNCS.clov_restart})
+		end
+	end
+	local clov_config_ui = { n = G.UIT.R, config = { align = "tm", padding = 0 }, nodes = { left_settings, right_settings } }
+	return {
+		n = G.UIT.ROOT,
+		config = {
+			emboss = 0.05,
+			minh = 6,
+			r = 0.1,
+			minw = 10,
+			align = "cm",
+			padding = 0.05,
+			colour = G.C.BLACK,
+		},
+		nodes = {
+			clov_config_ui
+		}
+	}
+end
+
+--end code being borrowed from Cardsauce
+
+--Atlas defs start 
 SMODS.Atlas {
     key = "PlaceHolder",
     path = "PlaceHolder.png",
@@ -32,7 +92,7 @@ SMODS.Atlas {
     px = 71,
     py = 95
 }
-
+--Atlas defs end 
 
 SMODS.Joker {
     key = 'lodog',
